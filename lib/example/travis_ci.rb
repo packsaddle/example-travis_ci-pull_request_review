ddc8deb1b8827f82454c576c46a6af3bfae03499
sanemat <o.gata.ken@gmail.com>
2015-02-25 01:43:11 +0900

    chore(invalid): add too long line

--- lib/example/travis_ci.rb
+++ lib/example/travis_ci.rb
@@ -3,5 +3,8 @@ require 'example/travis_ci/version'
 module Example
   module TravisCi
     # Your code goes here...
+    def too_long
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    end
   end
 end
