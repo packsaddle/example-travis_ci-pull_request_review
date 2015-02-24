379003a0dcb6f5f44a61c6a9271610705440ba84
sanemat <o.gata.ken@gmail.com>
2015-02-25 00:53:35 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in example-travis_ci.gemspec
+gemspec
---
+++ lib/example/travis_ci.rb
@@ -0,0 +1,7 @@
+require "example/travis_ci/version"
+
+module Example
+  module TravisCi
+    # Your code goes here...
+  end
+end
