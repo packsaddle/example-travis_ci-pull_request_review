d1dd280bd377593e0486149baf1dc6c67856cb9e
sanemat <o.gata.ken@gmail.com>
2015-02-25 02:10:48 +0900

    debug

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -2,6 +2,8 @@
 set -v
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   git branch -r
+  git remote show
+  git remote show origin
   gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
               checkstyle_filter-git saddler saddler-reporter-github
   git diff -z --name-only ..origin/master \
