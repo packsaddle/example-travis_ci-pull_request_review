42f5e1c72291f8a25856c5799ad555462848cd28
sanemat <o.gata.ken@gmail.com>
2015-02-25 01:37:29 +0900

    chore(saddler): setting saddler

---
+++ bin/run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -v
+if [ -v "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  gem install rubocop-select rubocop rubocop-formatter-checkstyle_formatter \
+              checkstyle_filter-git saddler saddler-reporter-github
+  git diff -z --name-only ..origin/master \
+   | xargs -0 rubocop-select \
+   | xargs rubocop \
+       --require rubocop/formatter/checkstyle_formatter \
+       --format RuboCop::Formatter::CheckstyleFormatter \
+   | checkstyle_filter-git diff ..origin/master \
+   | saddler report \
+      --require saddler/reporter/github \
+      --reporter Saddler::Reporter::Github::PullRequestReviewComment
+fi
+bundle exec rake
