49033b8ad40980ba7cd4ade3275763427b4b5dce
sanemat <o.gata.ken@gmail.com>
2015-02-25 20:17:08 +0900

    fix comment out

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -v
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  // Travis-CI
-  //
-  // git clone --depth=50 \
-  // git://github.com/packsaddle/example-ruby-travis-ci.git \
-  // packsaddle/example-ruby-travis-ci
-  // cd packsaddle/example-ruby-travis-ci
-  // git fetch origin +refs/pull/1/merge:
-  // git checkout -qf FETCH_HEAD
+  # Travis-CI
+  #
+  # git clone --depth=50 \
+  # git://github.com/packsaddle/example-ruby-travis-ci.git \
+  # packsaddle/example-ruby-travis-ci
+  # cd packsaddle/example-ruby-travis-ci
+  # git fetch origin +refs/pull/1/merge:
+  # git checkout -qf FETCH_HEAD
 
   gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
               checkstyle_filter-git saddler saddler-reporter-github
