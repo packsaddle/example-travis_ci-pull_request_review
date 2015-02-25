95511a278ab699d445916364a95a68d06c2dd52d
sanemat <o.gata.ken@gmail.com>
2015-02-25 18:59:46 +0900

    show steps

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -8,6 +8,25 @@ if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; t
 
   gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
               checkstyle_filter-git saddler saddler-reporter-github
+
+  git diff -z --name-only ..origin/master
+
+  git diff -z --name-only ..origin/master \
+   | xargs -0 rubocop-select
+
+  git diff -z --name-only ..origin/master \
+   | xargs -0 rubocop-select \
+   | xargs rubocop \
+       --require rubocop/formatter/checkstyle_formatter \
+       --format RuboCop::Formatter::CheckstyleFormatter
+
+  git diff -z --name-only ..origin/master \
+   | xargs -0 rubocop-select \
+   | xargs rubocop \
+       --require rubocop/formatter/checkstyle_formatter \
+       --format RuboCop::Formatter::CheckstyleFormatter \
+   | checkstyle_filter-git diff ..origin/master
+
   git diff -z --name-only ..origin/master \
    | xargs -0 rubocop-select \
    | xargs rubocop \
